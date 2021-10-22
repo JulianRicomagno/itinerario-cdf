c1d1e74fb97a7ac981af0a7dbe148b8130fef22b
Julian <julianricomagno@gmail.com>
2021-10-22 19:01:51 -0300

    CDF - 12 | Home / Itinerary - incorporando navegaciones de pantallas

--- app/screens/Attractions/CreateItineraty.js
+++ app/screens/Attractions/CreateItineraty.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import {View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {View, Button, Platform, Text, TextInput} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function CreateItineraty() {
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+      };
+    
+      const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+      };
+    
+      const showDatepicker = () => {
+        showMode('date');
+      };
+    
+      const showTimepicker = () => {
+        showMode('time');
+      };
+
     return (
         <View>
             <Text> Aca se crea el Itinerario, se define las fecha y el hotel</Text>
-        </View>
+
+            <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+    <Text
+    onPress={showTimepicker}
+    >Elija una fecha</Text>
+
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+
+
+
+        </View>     
     )
 }
--- app/screens/Attractions/MyTrip.js
+++ app/screens/Attractions/MyTrip.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {View, Text } from 'react-native';
 
 export default function MyTrip() {
+
+    
     return (
         <View>
             <Text>Aca va el listado de dias con las atracciones guardadas</Text>
